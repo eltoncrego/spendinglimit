11268ecfabca843af627289f0b5409d48de7ca8d
Elton C. Rego <rego.elton@gmail.com>
2018-07-24 20:20:28 -0700

    v1.4.1 transactions clear on a new week and weeks reset at midnight

--- src/func/navigation.js
+++ src/func/navigation.js
@@ -87,9 +87,23 @@ export default class App extends Component {
     });
   }
 
+  refreshTransactions() {
+    const that = this;
+    this.retrieveItem('currentTransactions').then((data) => {
+      console.log("Transactions synced: ");
+      console.log(JSON.parse(data));
+      that.setState({
+        currentTransactions: data == null ? [] : JSON.parse(data),
+      });
+    }).catch((error) => {
+      alert(error.message);
+    });
+  }
+
   render() {
     if(this.state.spendinglimit__checked != null){
-      if(this.state.spendinglimit_expiration.getTime() <= this.state.today.getTime()){
+      if(this.state.spendinglimit_expiration.setHours(0,0,0,0) <= this.state.today.getTime()){
+        this.refreshTransactions();
         return <SetLimit/>;
       } else if (this.state.spendinglimit__set){
         return <LimitSet/>;
